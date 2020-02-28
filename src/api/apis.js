ade80e68896b0f7344ddd5c27105415294c4230c
Mingming <peiliming910@gmail.com>
2020-02-28 14:41:48 +0900

    vue-axios サーバ環境修正

--- src/api/apis.js
+++ src/api/apis.js
@@ -2,7 +2,7 @@ import { fetch } from './fetch'
 
 export function getTestData() {
     return fetch({
-        url: '/test',   // vue.config.js mockでjsonを読み込ませる時に urlを一致にする必要がある
+        url: '/search',   // vue.config.js mockでjsonを読み込ませる時に urlを一致にする必要がある
         method: 'get'
     })
 }
--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,35 @@
-//const test = require('./data/data.json');
-//const test = require('https://rbai.jrt-desk.com/data/data.json');
+const test = require('./apis/data.json');
+//const test2 = require('./apis/data.json');
 
 module.exports = {
     //
     devServer: {
+        open: true,
         port: 8080,
         before(app) {
-            app.get("/test", (req, res) => {   //localhost:8080/api/index   data.jsonを読み込み
-        //    app.get("https://rbai.jrt-desk.com/data", (req, res) => {
-                res.json('https://rbai.jrt-desk.com/data/data.json');
+            app.get("/search", (req, res) => {   //localhost:8080/api/index   data.jsonを読み込み
+                //    app.get("https://rbai.jrt-desk.com/data", (req, res) => {
+                res.json(test);
             });
+        },
+        proxy: {
+            '/apis': {
+                target: 'https://movie.douban.com/',  // target host
+                ws: true,  // proxy websockets
+                changeOrigin: true,  // needed for virtual hosted sites
+                pathRewrite: {
+                    '^/apis': ''  // rewrite path
+                },
+                before(app) {
+                    app.get('/apis/ithil_j/activity/movie_annual2017')
+                        .then(function (res) {
+                            console.log(res.data)
+                        })
+                        .catch(function (err) {
+                            console.log(err)
+                        })
+                }
+            }
         }
     }
 };
